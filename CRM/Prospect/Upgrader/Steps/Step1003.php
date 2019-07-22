7783357565f1d07ea1f7a450b7e27e0a8691e1d5
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-07-22 17:51:17 +0100

    RSE-231: Add upgrader to move the prospect custom fields to the prospect case category.

---
+++ CRM/Prospect/Upgrader/Steps/Step1003.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Migrates the Prospect Custom fields to the prospecting case category.
+ */
+class CRM_Prospect_Upgrader_Steps_Step1003 {
+
+  /**
+   * Migrates the Prospect Custom fields to the prospecting case category.
+   *
+   * @return bool
+   *   Return value in boolean.
+   */
+  public function apply() {
+    $prospectCustomroups = [
+      'Prospect_Financial_Information',
+      'Prospect_Substatus',
+    ];
+
+    $result = civicrm_api3('CustomGroup', 'get', [
+      'return' => ['id'],
+      'name' => ['IN' => $prospectCustomroups],
+      'extends' => 'Case',
+    ]);
+
+    if ($result['count'] == 0) {
+      return TRUE;
+    }
+
+    foreach ($result['values'] as $value) {
+      civicrm_api3('CustomGroup', 'create', [
+        'id' => $value['id'],
+        'extends' => 'prospecting',
+      ]);
+    }
+
+    return TRUE;
+  }
+
+}
