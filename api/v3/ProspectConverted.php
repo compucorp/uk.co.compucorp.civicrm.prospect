414aa613a340ba4f2cf36705827f4588450b9ca3
Debarshi Bhaumik <deb1990@gmail.com>
2019-07-31 12:29:45 +0530

    RSE-196: Refactor getpaymentinfo API

--- api/v3/ProspectConverted.php
+++ api/v3/ProspectConverted.php
@@ -1,58 +1,88 @@
 <?php
 
 /**
- * ProspectConverted.create API specification (optional)
- * This is used for documentation and validation.
- *
- * @param array $spec description of fields supported by this API call
- * @return void
- * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ * @file
+ * Prospect Converted API.
  */
-function _civicrm_api3_prospect_converted_create_spec(&$spec) {
-  // $spec['some_parameter']['api.required'] = 1;
-}
 
 /**
- * ProspectConverted.create API
+ * ProspectConverted.create API.
  *
  * @param array $params
- * @return array API result descriptor
+ *   Parameters.
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
-function civicrm_api3_prospect_converted_create($params) {
+function civicrm_api3_prospect_converted_create(array $params) {
   return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
 
 /**
- * ProspectConverted.delete API
+ * ProspectConverted.delete API.
  *
  * @param array $params
- * @return array API result descriptor
+ *   Parameters.
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
-function civicrm_api3_prospect_converted_delete($params) {
+function civicrm_api3_prospect_converted_delete(array $params) {
   return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
 
 /**
- * ProspectConverted.get API
+ * ProspectConverted.get API.
  *
  * @param array $params
- * @return array API result descriptor
+ *   Parameters.
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
-function civicrm_api3_prospect_converted_get($params) {
+function civicrm_api3_prospect_converted_get(array $params) {
   return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
 
 /**
- * ProspectConverted.getpaymentinfo API
+ * ProspectConverted.getpaymentinfo Spec.
  *
  * @param array $params
- * @return array API result descriptor
+ *   Parameters.
+ */
+function _civicrm_api3_prospect_converted_getpaymentinfo_spec(array &$params) {
+  $params['prospect_case_id'] = [
+    'title' => 'Prospect Case ID',
+    'description' => 'Prospect Case ID',
+    'api.required' => TRUE,
+    'type' => CRM_Utils_Type::T_INT,
+  ];
+}
+
+/**
+ * ProspectConverted.getpaymentinfo API.
+ *
+ * @param array $params
+ *   Parameters.
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
-function civicrm_api3_prospect_converted_getpaymentinfo($params) {
+function civicrm_api3_prospect_converted_getpaymentinfo(array $params) {
   $prospectConverted = CRM_Prospect_BAO_ProspectConverted::findByCaseID($params['prospect_case_id']);
-  return $prospectConverted->getPaymentInfo();
+  $returnValue = [];
+
+  if ($prospectConverted) {
+    $returnValue = $prospectConverted->getPaymentInfo();
+  }
+
+  return $returnValue;
 }
