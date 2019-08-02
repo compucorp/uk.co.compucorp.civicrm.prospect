74ccaae4c8776816a4d93b1021b4fe680425f58f
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-08-02 20:31:36 +0100

    RSE-249: Linter Fixes.

--- CRM/Prospect/Form/Handler/CaseStatusUpdate.php
+++ CRM/Prospect/Form/Handler/CaseStatusUpdate.php
@@ -4,23 +4,27 @@ use CRM_Prospect_CustomFieldsFormBuilder as CustomFieldsFormBuilder;
 use CRM_Prospect_Helper_CaseTypeCategory as CaseTypeCategoryHelper;
 
 /**
- * Implementation of custom handler executed within prospect_civicrm_buildForm
- * hook.
+ * Implementation of custom handler executed within buildForm hook.
  */
 class CRM_Prospect_Form_Handler_CaseStatusUpdate {
+
   /**
+   * Handle function.
+   *
    * Adds Specified 'Prospect_Financial_Information' and 'Prospect_Substatus'
    * custom groups fields to Case status update form.
    *
    * @param string $formName
+   *   Form name.
    * @param object $form
+   *   Form object.
    */
   public function handle($formName, $form) {
     if (!$this->canHandle($formName, $form)) {
       return;
     }
 
-    // builds (Financial Information) Fields
+    // Builds (Financial Information) Fields.
     $fieldsToShow = [
       'Prospect_Amount',
       'Probability',
@@ -32,7 +36,7 @@ class CRM_Prospect_Form_Handler_CaseStatusUpdate {
     $prospectFinancialInformationFieldsForm = $customFieldsFormBuilder->buildFieldsForm('Prospect_Financial_Information', $fieldsToShow, CRM_Utils_Request::retrieve('caseid', 'Integer'));
     $form->assign('prospectFinancialInformationFieldsForm', $prospectFinancialInformationFieldsForm->toSmarty());
 
-    // builds (Substatus) Fields
+    // Builds (Substatus) Fields.
     $fieldsToShow = [
       'Substatus',
     ];
@@ -45,9 +49,12 @@ class CRM_Prospect_Form_Handler_CaseStatusUpdate {
    * Checks if we are on add Case Activity form.
    *
    * @param string $formName
+   *   Form name.
    * @param object $form
+   *   Form object.
    *
    * @return bool
+   *   Returns boolean.
    */
   private function canHandle($formName, $form) {
     $isCaseActivityForm = $formName === 'CRM_Case_Form_Activity';
--- CRM/Prospect/Form/Handler/OpenCase.php
+++ CRM/Prospect/Form/Handler/OpenCase.php
@@ -3,25 +3,26 @@
 use CRM_Prospect_CustomFieldsFormBuilder as CustomFieldsFormBuilder;
 
 /**
- * Implementation of custom handler for open case form
- * executed within prospect_civicrm_buildForm hook.
+ * Implementation of custom handler for open case form.
+ *
+ * This is executed within prospect_civicrm_buildForm hook.
  */
 class CRM_Prospect_Form_Handler_OpenCase {
 
   /**
-   * Contains the form object provided
-   * by buildForm hook.
+   * Contains the form object provided by buildForm hook.
    *
    * @var object
    */
   private $form;
 
   /**
-   * Executes custom changes to new/update
-   * case form.
+   * Executes custom changes to new/update case form.
    *
    * @param string $formName
+   *   Form name.
    * @param object $form
+   *   Form object.
    */
   public function handle($formName, $form) {
     if (!$this->canHandle($formName, $form)) {
@@ -34,17 +35,23 @@ class CRM_Prospect_Form_Handler_OpenCase {
   }
 
   /**
-   * Checks if this page is 'Open Case'
-   * form.
+   * Checks if this page is 'Open Case' form.
    *
    * @param string $formName
+   *   Form name.
+   * @param CRM_Core_Form $form
+   *   Form object.
    *
    * @return bool
+   *   Returns boolean.
    */
   private function canHandle($formName, CRM_Core_Form $form) {
     return $formName == 'CRM_Case_Form_Case' && $this->isProspectCategory($form);
   }
 
+  /**
+   * Add the substatus custom group.
+   */
   private function addSubstatusCustomGroup() {
     $fieldsToShow = [
       'Substatus',
@@ -55,6 +62,7 @@ class CRM_Prospect_Form_Handler_OpenCase {
 
     $prospectSubstatusFieldsForm = $customFieldsFormBuilder->buildFieldsForm('Prospect_Substatus', $fieldsToShow);
     $this->form->assign('prospectSubstatusFieldsForm', $prospectSubstatusFieldsForm->toSmarty());
+  }
 
   /**
    * Checks if the case is of prospect category.
@@ -71,4 +79,5 @@ class CRM_Prospect_Form_Handler_OpenCase {
 
     return !empty($urlParams['case_type_category']) && $urlParams['case_type_category'] == 'prospecting';
   }
+
 }
