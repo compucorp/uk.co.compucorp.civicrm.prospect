c8955b91fdb8c528960609e6703269df6bdbfd4b
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-08-27 17:06:40 +0100

    RSE-271: Adds the prospect category word replacement class

---
+++ CRM/Prospect/WordReplacement/ProspectCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+use CRM_Prospect_ExtensionUtil as ExtensionUtil;
+
+/**
+ * Class ProspectCategory.
+ */
+class CRM_Prospect_WordReplacement_ProspectCategory implements CRM_Civicase_WordReplacement_BaseInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return array
+   *   Returns the word replacements
+   */
+  public function get() {
+    $configFile = CRM_Core_Resources::singleton()
+      ->getPath(ExtensionUtil::LONG_NAME, 'config/word_replacement/prospect_category.php');
+
+    return include $configFile;
+  }
+
+}
