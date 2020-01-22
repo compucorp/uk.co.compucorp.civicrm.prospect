acf83ce77e0df195321a72eec76d09aadef023d0
Debarshi Bhaumik <deb1990@gmail.com>
2020-01-22 07:22:10 +0000

    RSE-765: Remove campaign field using upgrader

---
+++ CRM/Prospect/Upgrader/Steps/Step1007.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Class CRM_Prospect_Upgrader_Steps_Step1007.
+ */
+class CRM_Prospect_Upgrader_Steps_Step1007 {
+
+  /**
+   * Removes the 'Campaign_Id' Custom Field
+   *
+   * @return bool
+   *   Return value in boolean.
+   */
+  public function apply() {
+    $result = civicrm_api3('CustomField', 'getsingle', [
+      'return' => ["id"],
+      'custom_group_id' => "Prospect_Financial_Information",
+      'name' => "Campaign_Id",
+    ]);
+
+    if ($result['id']) {
+      civicrm_api3('CustomField', 'delete', ['id' => $result['id']]);
+    }
+
+    return TRUE;
+  }
+
+}
