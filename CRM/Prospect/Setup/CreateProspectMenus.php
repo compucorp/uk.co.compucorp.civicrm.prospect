df50022e778e56ed3db418455bc22b30126371e5
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-05-25 12:02:32 +0100

    RSE-763: Set proper permission for prospect menus

--- CRM/Prospect/Setup/CreateProspectMenus.php
+++ CRM/Prospect/Setup/CreateProspectMenus.php
@@ -40,7 +40,7 @@ class CRM_Prospect_Setup_CreateProspectMenus {
       'url' => NULL,
       'permission_operator' => 'OR',
       'is_active' => 1,
-      'permission' => 'access my cases and activities,access all cases and activities',
+      'permission' => 'access my prospecting and activities,access all prospecting and activities',
       'icon' => 'crm-i fa-folder-open-o',
     ];
 
@@ -66,21 +66,21 @@ class CRM_Prospect_Setup_CreateProspectMenus {
         'label' => ts('Dashboard'),
         'name' => 'prospect_dashboard',
         'url' => 'civicrm/case/a/?case_type_category=prospecting#/case?case_type_category=prospecting',
-        'permission' => 'access my cases and activities,access all cases and activities',
+        'permission' => 'access my prospecting and activities,access all prospecting and activities',
         'permission_operator' => 'OR',
       ],
       [
         'label' => ts('New Prospect'),
         'name' => 'new_prospect',
         'url' => 'civicrm/case/add?case_type_category=prospecting&action=add&reset=1&context=standalone',
-        'permission' => 'add cases,access all cases and activities',
+        'permission' => 'add prospecting,access all prospecting and activities',
         'permission_operator' => 'OR',
       ],
       [
         'label' => ts('Manage Prospects'),
         'name' => 'manage_prospect',
         'url' => 'civicrm/case/a/?case_type_category=prospecting#/case/list?cf=%7B"case_type_category":"prospecting"%7D',
-        'permission' => 'access my cases and activities,access all cases and activities',
+        'permission' => 'access my prospecting and activities,access all prospecting and activities',
         'permission_operator' => 'OR',
       ],
     ];
