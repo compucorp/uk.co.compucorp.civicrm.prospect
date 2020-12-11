706c22c59ad5ec9ba086502caf6a7b222a25f3fa
shahrukh-compucorp <shahrukh@compucorp.co.uk>
2020-12-11 18:50:27 +0500

    CPS-349: Update case type category for prospecting through manual query

--- CRM/Prospect/Setup/CreateProspectWorkflowCaseType.php
+++ CRM/Prospect/Setup/CreateProspectWorkflowCaseType.php
@@ -26,7 +26,7 @@ class CRM_Prospect_Setup_CreateProspectWorkflowCaseType {
       'activitySets' => $this->getActivitySets(),
     ];
 
-    civicrm_api3('CaseType', 'create', [
+    $result = civicrm_api3('CaseType', 'create', [
       'name' => 'default_prospect_workflow',
       'title' => 'Default Prospect Workflow',
       'description' => "The Default Prospect Workflow Case Type",
@@ -34,6 +34,46 @@ class CRM_Prospect_Setup_CreateProspectWorkflowCaseType {
       'is_active' => 0,
       'case_type_category' => 'Prospecting',
     ]);
+    // Updating through manual query is unavoidable here
+    // as case_type_category is not a core field and it does
+    // not gets filled through api during installation.
+    $this->updateCaseTypeCategoryToProspecting($result['id']);
+  }
+
+  /**
+   * Updates the case type category to "Prospecting".
+   *
+   * @param int $caseTypeId
+   *   Case Type Id.
+   */
+  private function updateCaseTypeCategoryToProspecting($caseTypeId) {
+    $caseTypeTable = CRM_Case_BAO_CaseType::getTableName();
+    $caseCategoryOptionValue = $this->getCaseCategoryOptionValueForProspecting();
+    CRM_Core_DAO::executeQuery(
+      "UPDATE {$caseTypeTable} SET case_type_category = %1 WHERE id = $caseTypeId",
+      [1 => [$caseCategoryOptionValue, 'Integer']]
+    );
+  }
+
+  /**
+   * Returns the Case category option value.
+   *
+   * @return int|null
+   *   Case category value.
+   */
+  private function getCaseCategoryOptionValueForProspecting() {
+    $result = civicrm_api3('OptionValue', 'get', [
+      'sequential' => 1,
+      'option_group_id' => 'case_type_categories',
+      'name' => CRM_Prospect_Helper_CaseTypeCategory::PROSPECT_CASE_TYPE_CATEGORY_NAME,
+      'return' => ['value'],
+    ]);
+
+    if ($result['count'] == 0) {
+      return;
+    }
+
+    return $result['values'][0]['value'];
   }
 
   /**
