c235ee6b2895f58bf619c652764e68cc87bc033d
colemanw <coleman@civicrm.org>
2026-09-22 13:14:56 -0400

    Convert to PHP short array syntax
    Auto-convert `array(...)` to `[...]` for nicer-looking PHP code :)
    
    The script I used for this can be found here: https://github.com/thomasbachem/php-short-array-syntax-converter

--- xml/schema/CRM/Prospect/ProspectConverted.entityType.php
+++ xml/schema/CRM/Prospect/ProspectConverted.entityType.php
@@ -1,11 +1,11 @@
 <?php
 // This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
-return array (
+return [
   0 => 
-  array (
+  [
     'name' => 'ProspectConverted',
     'class' => 'CRM_Prospect_DAO_ProspectConverted',
     'table' => 'civicrm_prospect_converted',
-  ),
-);
+  ],
+];
